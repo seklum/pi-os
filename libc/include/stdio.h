9af04eeca8a031840f3abbdbc3ef3c45053d8123
Charles <charles.h7272@gmail.com>
2015-02-25 17:34:44 -0700

    Third Commit

---
+++ libc/include/stdio.h
@@ -0,0 +1,13 @@
+#ifndef _STDIO_H
+#define _STDIO_H 1
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ libc/string/strlen.c
@@ -0,0 +1,8 @@
+#include <stdint.h>
+#include <string.h>
+
+size_t strlen(const char* str)
+{
+	size_t iter;
+	for(iter = 0; str[iter]; ++iter;
+}
